0a6874861707d383b81d9fecae15f8d6589f7dc6
tobitubbie <triplttt@gmail.com>
2023-06-16 14:04:28 +0200

    refactors global-container + makes instantiation easier: moves injection from constructor to field + removes unnecessary overridden function

--- libs/ng-key-master/src/lib/global-container.ts
+++ libs/ng-key-master/src/lib/global-container.ts
@@ -1,27 +1,23 @@
-import {Inject, Injectable} from "@angular/core";
+import {inject, Injectable} from "@angular/core";
 import {Container} from "./container";
-import {GlobalContainerConfig} from "./models";
 import {GLOBAL_CONTAINER_CONFIG} from "./tokens";
 import {NoopStrategy} from "./strategies";
 
 
-@Injectable({ providedIn: 'root'})
+@Injectable({providedIn: 'root'})
 export class GlobalContainer extends Container {
-  override ignoreTargets = this.globalConfig.ignoreTargets;
-  override name = this.globalConfig.name;
-  override element = this.globalConfig.element;
+
+  #config = inject(GLOBAL_CONTAINER_CONFIG);
+
+  override ignoreTargets = this.#config.ignoreTargets;
+  override name = this.#config.name;
+  override element = this.#config.element;
   override strategy = new NoopStrategy();
 
-  constructor(
-    @Inject(GLOBAL_CONTAINER_CONFIG) private readonly globalConfig: GlobalContainerConfig,
-  ) {
+  constructor() {
     super();
     this.element.addEventListener('keydown', (event) => {
       if (event instanceof KeyboardEvent) this.onKeyboardEvent(event);
     });
-  }
-
-  override onKeyboardEvent(event: KeyboardEvent) {
-    super.onKeyboardEvent(event);
   }
 }
